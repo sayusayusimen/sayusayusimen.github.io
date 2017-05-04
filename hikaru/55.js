46be9f80fe4bfb8470fd57411011d5bd68df6f6b
3haruka14 <sayu_gekka@yahoo.co.jp>
2017-05-04 23:43:40 +0900

    3

--- hikaru/55.js
+++ hikaru/55.js
@@ -1,5 +1,6 @@
 var now_date = new Date();
 $.each({
+    '56.html':['2017/5/4','2017/5/4 23:59:59'],
     '56.html':['2017/5/6','2018/5/4 23:59:59'],
 },function(redirect_url,date_time){
     if(new Date(date_time[0]) <= now_date && now_date <= new Date(date_time[1])){
