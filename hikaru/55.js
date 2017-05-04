c7036768fd2482ce27acd74c8cdb94eb1ab8c2a1
3haruka14 <sayu_gekka@yahoo.co.jp>
2017-05-04 23:42:55 +0900

    二回目

--- hikaru/55.js
+++ hikaru/55.js
@@ -1,6 +1,6 @@
 var now_date = new Date();
 $.each({
-    '55.html':['2017/5/5','2017/5/5 23:59:59'],
+    '56.html':['2017/5/6','2018/5/4 23:59:59'],
 },function(redirect_url,date_time){
     if(new Date(date_time[0]) <= now_date && now_date <= new Date(date_time[1])){
         //対象日時の場合
